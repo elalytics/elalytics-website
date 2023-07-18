ed33cf89122626053c7880a09e2c4bfeeb0af7a4
ddeepak95 <ddeepak95@gmail.com>
2023-07-18 15:49:46 -0700

    character sentiment chart

--- src/app/the-boy/sentiment-chart/components/CharacterSentimentChart.js
+++ src/app/the-boy/sentiment-chart/components/CharacterSentimentChart.js
@@ -1,19 +1,38 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Chart } from "chart.js/auto";
 
-const BarChart = (props) => {
+const CharacterSentimentChart = (props) => {
+  const [chartData, setChartData] = useState();
   const chartContainer = useRef(null);
   const chartInstance = useRef(null);
-  const options = {
-    indexAxis: "y",
-    scales: {
-      y: {
-        beginAtZero: true,
-      },
-    },
-    responsive: true,
-  };
+
+  useEffect(() => {
+    let data = {
+      labels: props.data.map((characterData) => characterData.character),
+      datasets: [
+        {
+          label: "Positive",
+          data: props.data.map(
+            (characterData) => characterData.positive.length
+          ),
+          backgroundColor: "rgb(74 222 128 / 0.4)",
+          borderColor: "rgb(74 222 128)",
+          borderWidth: 1,
+        },
+        {
+          label: "Negative",
+          data: props.data.map(
+            (characterData) => characterData.negative.length
+          ),
+          backgroundColor: "rgb(248 113 113 / 0.4)",
+          borderColor: "rgb(248 113 113)",
+          borderWidth: 1,
+        },
+      ],
+    };
+    setChartData(data);
+  }, [props.data]);
 
   useEffect(() => {
     if (chartContainer && chartContainer.current) {
@@ -25,21 +44,58 @@ const BarChart = (props) => {
 
       chartInstance.current = new Chart(myChartRef, {
         type: "bar",
-        data: props.data,
-        options: options,
+        data: chartData,
+        options: {
+          indexAxis: "y",
+          scales: {
+            y: {
+              beginAtZero: true,
+            },
+          },
+          plugins: {
+            tooltip: {
+              mode: "nearest",
+              intersect: true,
+              callbacks: {
+                label: function (tooltipItem) {
+                  if (tooltipItem.dataset.label === "Positive") {
+                    return (
+                      tooltipItem.dataset.label +
+                      ": " +
+                      tooltipItem.raw +
+                      " (" +
+                      props.data[tooltipItem.dataIndex].positive +
+                      ")"
+                    );
+                  } else if (tooltipItem.dataset.label === "Negative") {
+                    return (
+                      tooltipItem.dataset.label +
+                      ": " +
+                      tooltipItem.raw +
+                      " (" +
+                      props.data[tooltipItem.dataIndex].negative +
+                      ")"
+                    );
+                  }
+                },
+              },
+            },
+          },
+          responsive: true,
+        },
       });
     }
-  }, [chartContainer, props.data, options]);
+  }, [chartContainer, chartData]);
 
-  if (props.data === undefined || props.data === null) {
+  if (chartData === undefined || chartData === null) {
     return <div>loading</div>;
   }
 
   return (
-    <div style={{ width: "500px" }}>
+    <div style={{ width: "100%" }}>
       <canvas ref={chartContainer} />
     </div>
   );
 };
 
-export default BarChart;
+export default CharacterSentimentChart;
--- src/app/the-boy/sentiment-chart/page.js
+++ src/app/the-boy/sentiment-chart/page.js
@@ -1,80 +1,178 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import BarChart from "./components/CharacterSentimentChart";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { Tooltip } from "react-tooltip";
+import characterWordsData from "./data/characterData.json";
+import wordSentimentList from "./data/wordssentiment.json";
+import CharacterSentimentChart from "./components/CharacterSentimentChart";
 
-const rawData = [
-  { character: "Mother", words: ["able", "hello", "how"] },
-  { character: "Head Master", words: ["able", "sure", "old", "hero"] },
-];
+//filter words by sentiment
+function filterWords(wordSentimentList, sentiment) {
+  return wordSentimentList
+    .filter((word) => word.sentiment === sentiment)
+    .map((item) => item.word);
+}
 
-const wordList = [
-  { word: "able", positive: true },
-  { word: "hello", positive: true },
-  { word: "how", positive: false },
-  { word: "sure", positive: false },
-  { word: "old", positive: true },
-  { word: "hero", positive: false },
-];
+function findCharacterSentiments(charData, positive, negative) {
+  return charData
+    .filter((c) => c.words.length > 0) // skip characters that doesn't have any words
+    .map((c) => {
+      let positiveWords = [];
+      let negativeWords = [];
+      let unknownWords = [];
 
-function countCharacterPositiveAndNegativeWords(rawData, wordList) {
-  return rawData.map((characterData) => {
-    let positiveWordsCount = 0;
-    let negativeWordsCount = 0;
+      c.words.forEach((word) => {
+        if (positive.includes(word)) positiveWords.push(word);
+        else if (negative.includes(word)) negativeWords.push(word);
+        else unknownWords.push(word);
+      });
 
-    characterData.words.forEach((word) => {
-      let wordData = wordList.find((wordItem) => wordItem.word === word);
-      if (wordData.positive === true) {
-        positiveWordsCount++;
-      } else {
-        negativeWordsCount++;
-      }
+      return {
+        character: c.character,
+        positive: positiveWords,
+        negative: negativeWords,
+        unknown: unknownWords,
+      };
     });
-
-    return {
-      character: characterData.character,
-      positive: positiveWordsCount,
-      negative: negativeWordsCount,
-    };
-  });
 }
 
 export default function Home() {
   const [characterData, setCharacterData] = useState([]);
-  const [chartData, setChartData] = useState();
+  const [positiveWords, setPositiveWords] = useState([]);
+  const [negativeWords, setNegativeWords] = useState([]);
+
+  const handlePositiveWordClick = (word) => {
+    setPositiveWords(
+      positiveWords.filter((positiveWord) => positiveWord !== word)
+    );
+    setNegativeWords([...negativeWords, word]);
+  };
+
+  const handleNegativeWordClick = (word) => {
+    setNegativeWords(
+      negativeWords.filter((negativeWord) => negativeWord !== word)
+    );
+    setPositiveWords([...positiveWords, word]);
+  };
+
+  function findCharactersByWord(array, word) {
+    let result = [];
+    array.forEach((character) => {
+      if (
+        character.positive.includes(word) ||
+        character.negative.includes(word) ||
+        character.unknown.includes(word)
+      ) {
+        result.push(character.character);
+      }
+    });
+    return result;
+  }
 
   useEffect(() => {
-    setCharacterData(countCharacterPositiveAndNegativeWords(rawData, wordList));
+    setPositiveWords(filterWords(wordSentimentList, "positive"));
+    setNegativeWords(filterWords(wordSentimentList, "negative"));
   }, []);
   useEffect(() => {
-    console.log(characterData);
+    setCharacterData(
+      findCharacterSentiments(characterWordsData, positiveWords, negativeWords)
+    );
+  }, [positiveWords, negativeWords]);
+  useEffect(() => {
     let data = {
       labels: characterData.map((characterData) => characterData.character),
       datasets: [
         {
           label: "Positive",
-          data: characterData.map((characterData) => characterData.positive),
-          backgroundColor: "rgba(255, 99, 132, 0.2)",
-          borderColor: "rgb(255, 99, 132)",
+          data: characterData.map(
+            (characterData) => characterData.positive.length
+          ),
+          backgroundColor: "rgb(74 222 128 / 0.4)",
+          borderColor: "rgb(74 222 128)",
           borderWidth: 1,
         },
         {
           label: "Negative",
-          data: characterData.map((characterData) => characterData.negative),
-          backgroundColor: "rgba(123, 99, 132, 0.2)",
-          borderColor: "rgb(123, 99, 132)",
+          data: characterData.map(
+            (characterData) => characterData.negative.length
+          ),
+          backgroundColor: "rgb(248 113 113 / 0.4)",
+          borderColor: "rgb(248 113 113)",
           borderWidth: 1,
         },
       ],
     };
-    setChartData(data);
   }, [characterData]);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10">
-        <BarChart data={chartData} />
+        <div className={`flex justify-between gap-4 rounded w-full p-2 m-2`}>
+          <div className="flex-1 p-2 flex flex-wrap gap-2 content-start rounded border-2 border-slate-400">
+            {positiveWords.map((word, index) => (
+              <span className="text-sm rounded my-1.5" key={index}>
+                <span
+                  className="bg-green-400/40 p-2 rounded-l"
+                  data-tooltip-id="characters-info"
+                  data-tooltip-content={findCharactersByWord(
+                    characterData,
+                    word
+                  )}
+                >
+                  {word}
+                </span>
+                <span
+                  className="p-2 cursor-pointer bg-green-400/60 rounded-r"
+                  onClick={() => handlePositiveWordClick(word)}
+                  data-tooltip-id="move-btn-info"
+                  data-tooltip-content="Move to Negative words"
+                >
+                  <FontAwesomeIcon icon={faArrowRight} className="opacity-50" />
+                </span>
+              </span>
+            ))}
+          </div>
+          <div className="flex-1 p-2 flex flex-wrap gap-2 content-start rounded border-2 border-slate-400">
+            {negativeWords.map((word, index) => (
+              <span className="text-sm rounded my-1.5" key={index}>
+                <span
+                  className="p-2 cursor-pointer bg-red-400/60 rounded-l"
+                  onClick={() => handleNegativeWordClick(word)}
+                  data-tooltip-id="move-btn-info"
+                  data-tooltip-content="Move to Positive words"
+                >
+                  <FontAwesomeIcon icon={faArrowLeft} className="opacity-50" />
+                </span>
+                <span
+                  className="bg-red-400/40 p-2 rounded-r"
+                  data-tooltip-id="characters-info"
+                  data-tooltip-content={findCharactersByWord(
+                    characterData,
+                    word
+                  )}
+                >
+                  {word}
+                </span>
+              </span>
+            ))}
+          </div>
+          <Tooltip
+            id="move-btn-info"
+            variant="light"
+            opacity={0.9}
+            style={{ fontSize: "0.8em" }}
+          />
+          <Tooltip
+            id="characters-info"
+            variant="light"
+            opacity={0.9}
+            style={{ fontSize: "0.8em" }}
+          />
+        </div>
       </div>
+      <CharacterSentimentChart data={characterData} />
     </main>
   );
 }
