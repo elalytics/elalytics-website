15c57a7d72025295e091647a87444e1a44d62ce2
ddeepak95 <ddeepak95@gmail.com>
2023-08-02 20:31:31 -0700

    categorize visualization

---
+++ src/app/general/identify-author-style-poe-jackson/component/Categorize.js
@@ -0,0 +1,174 @@
+import React, { useEffect, useState } from "react";
+import { DndProvider, useDrag, useDrop } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+
+const ItemTypes = {
+  WORD: "word",
+};
+
+function backgroundColorSelect(category, currentBucket, checkBucket) {
+  if (checkBucket) {
+    return currentBucket === category ? "bg-green-300" : "bg-red-300";
+  } else {
+    return "bg-stone-300";
+  }
+}
+
+function WordOption({
+  text,
+  category,
+  currentBucket,
+  moveToBucket,
+  checkBucket,
+}) {
+  const [{ isDragging }, drag] = useDrag({
+    type: ItemTypes.WORD,
+    item: { text, category, currentBucket },
+    end: (item, monitor) => {
+      const dropResult = monitor.getDropResult();
+      console.log(item);
+      if (item && dropResult) {
+        moveToBucket(item.text, item.currentBucket, dropResult.name);
+      }
+    },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
+
+  return (
+    <div
+      ref={drag}
+      className={`${backgroundColorSelect(
+        category,
+        currentBucket,
+        checkBucket
+      )} px-4 py-2 m-1 h-10 rounded cursor-move flex-shrink-0 flex-grow-0`}
+      style={{ opacity: isDragging ? 0.5 : 1 }}
+    >
+      {text}
+    </div>
+  );
+}
+
+function getUniqueCategories(arr) {
+  return arr.reduce((acc, item) => {
+    if (!acc["All Words"]) {
+      acc["All Words"] = [...arr];
+    }
+    if (!acc[item.category]) {
+      acc[item.category] = [];
+    }
+    return acc;
+  }, {});
+}
+
+function Bucket({ name, words, acceptWord, checkBucketStatus, className }) {
+  const [, drop] = useDrop({
+    accept: ItemTypes.WORD,
+    drop: () => ({ name }),
+    collect: (monitor) => ({
+      isOver: monitor.isOver(),
+      canDrop: monitor.canDrop(),
+    }),
+  });
+
+  return (
+    <div className={className}>
+      <h1 className="text-xl font-bold">{name}</h1>
+      <div
+        className="flex flex-wrap content-start min-h-[200px] border-2 border-stone-400 bg-white rounded-md p-1"
+        ref={drop}
+      >
+        {words.map((word) => (
+          <WordOption
+            key={word.text}
+            text={word.text}
+            category={word.category}
+            currentBucket={name}
+            checkBucket={checkBucketStatus}
+            moveToBucket={acceptWord}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+const Categorize = ({ data }) => {
+  const [categoryBuckets, setCategoryBuckets] = useState(
+    getUniqueCategories(data)
+  );
+  const [checkWordStatus, setCheckWordStatus] = useState(false);
+
+  useEffect(() => {
+    categoryBuckets["All Words"].length === 0
+      ? setCheckWordStatus(true)
+      : setCheckWordStatus(false);
+  }, [categoryBuckets]);
+
+  const moveToBucket = (word, currentCategory, newCategory) => {
+    function move(data, text, sourceKey, destinationKey) {
+      // Clone original data to avoid side-effects
+      let clonedData = JSON.parse(JSON.stringify(data));
+
+      // find the object in source key array
+      let foundIndex = clonedData[sourceKey].findIndex(
+        (item) => item.text === text
+      );
+
+      if (foundIndex >= 0) {
+        // if object found
+        // move object from source key array to destination key array
+        let foundItem = clonedData[sourceKey][foundIndex];
+        clonedData[sourceKey].splice(foundIndex, 1); // remove object from source key array
+        clonedData[destinationKey].push(foundItem); // add object to destination key array
+      }
+
+      return clonedData;
+    }
+
+    setCategoryBuckets(
+      move(categoryBuckets, word, currentCategory, newCategory)
+    );
+  };
+
+  function getBucketWidth() {
+    const bucketCount = Object.keys(categoryBuckets).length;
+    if (bucketCount > 3) {
+      return "w-full sm:w-1/2 md:w-1/2 lg:w-1/3";
+    } else if (bucketCount === 3) {
+      return "w-full sm:w-1/2 md:w-1/2";
+    } else {
+      return "w-full";
+    }
+  }
+
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <Bucket
+        name="All Words"
+        words={categoryBuckets["All Words"]}
+        acceptWord={moveToBucket}
+        checkBucketStatus={false}
+        className="w-full p-2"
+      />
+      <div className="flex flex-wrap justify-between w-full">
+        {Object.keys(categoryBuckets).map((key) => {
+          return key === "All Words" ? null : (
+            <Bucket
+              key={key.toString()}
+              name={key}
+              words={categoryBuckets[key]}
+              checkBucketStatus={checkWordStatus}
+              acceptWord={moveToBucket}
+              className={`${getBucketWidth()} p-2`}
+            />
+          );
+        })}
+      </div>
+    </DndProvider>
+  );
+};
+
+export default Categorize;
---
+++ src/app/general/identify-author-style-poe-jackson/page.js
@@ -0,0 +1,45 @@
+"use client";
+
+import Categorize from "./component/Categorize";
+
+export default function Home() {
+  const data = [
+    { text: "raven", category: "Edgar Allen Poe" },
+    { text: "horror", category: "Edgar Allen Poe" },
+    { text: "night", category: "Edgar Allen Poe" },
+    { text: "heart", category: "Edgar Allen Poe" },
+    { text: "soul", category: "Edgar Allen Poe" },
+    { text: "love", category: "Edgar Allen Poe" },
+    { text: "dream", category: "Edgar Allen Poe" },
+    { text: "terror", category: "Edgar Allen Poe" },
+    { text: "madness", category: "Edgar Allen Poe" },
+    { text: "darkness", category: "Edgar Allen Poe" },
+    { text: "village", category: "Shirley Jackson" },
+    { text: "house", category: "Shirley Jackson" },
+    { text: "lottery", category: "Shirley Jackson" },
+    { text: "stone", category: "Shirley Jackson" },
+    { text: "tradition", category: "Shirley Jackson" },
+    { text: "people", category: "Shirley Jackson" },
+    { text: "family", category: "Shirley Jackson" },
+    { text: "black", category: "Shirley Jackson" },
+    { text: "town", category: "Shirley Jackson" },
+    { text: "ritual", category: "Shirley Jackson" },
+  ];
+  return (
+    <main>
+      <div className="h-screen overflow-x-hidden bg-gray-100">
+        <div className="my-8 text-center max-w-3xl m-auto">
+          <h1 className="text-3xl font-bold ">Identifying Author's Style</h1>
+          <p>
+            Drag the words to corresponding author's box to categorize them
+            based on their style. After categorizing all the words, you will be
+            able to see how many words you got right.
+          </p>
+        </div>
+        <div className="h-full max-w-5xl m-auto">
+          <Categorize data={data} />
+        </div>
+      </div>
+    </main>
+  );
+}
