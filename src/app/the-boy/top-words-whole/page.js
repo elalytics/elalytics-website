51581462ab1b27f1bff6ce49c88be9657755c727
ddeepak95 <ddeepak95@gmail.com>
2023-08-03 19:20:34 -0700

    frequent words with spanish

--- src/app/the-boy/top-words-whole/page.js
+++ src/app/the-boy/top-words-whole/page.js
@@ -1,12 +1,12 @@
 "use client";
 
-import WordCloudDraggable from "../../utils/charts/WordCloudDraggable";
+import WordCloudDraggableDualLang from "../../utils/charts/WordCloudDraggableDualLang";
 import { filterWordCloudData } from "@/app/utils/functions/general-functions";
 import data from "./data/topwordsPOS.json";
 
 export default function Home() {
   const wordsToRemove = [];
-  const categoriesToRemove = [];
+  const categoriesToRemove = ["PUNCT", "DET", "AUX", "PART"];
   return (
     <main>
       <div className="h-screen overflow-x-hidden bg-gray-100">
@@ -14,14 +14,16 @@ export default function Home() {
           <span className="px-4 py-1 bg-stone-600 rounded text-white inline-block mb-1 text-sm font-bold">
             Boy
           </span>
-          <h1 className="text-3xl font-bold ">Top 100 Words in the book</h1>
+          <h1 className="text-3xl font-bold ">
+            Most Frequent Words in the book
+          </h1>
           <p>
-            Drag the words to different positions and see if you can find any
+            Drag the words to different positions and see if you can make any
             new insights!
           </p>
         </div>
         <div className="h-full w-screen">
-          <WordCloudDraggable
+          <WordCloudDraggableDualLang
             data={filterWordCloudData(data, wordsToRemove, categoriesToRemove)}
             wordSizeMultiplier={1}
             scaleType="log"
---
+++ src/app/utils/charts/WordCloudDraggableDualLang.js
@@ -0,0 +1,244 @@
+import { useEffect, useRef, useState } from "react";
+import * as d3 from "d3";
+import d3Cloud from "d3-cloud";
+import { Tooltip } from "react-tooltip";
+import { forceCollide } from "d3-force";
+
+function findMaxMin(data) {
+  var minimum = data.reduce(
+    (min, p) => (p.value < min ? p.value : min),
+    data[0].value
+  );
+  var maximum = data.reduce(
+    (max, p) => (p.value > max ? p.value : max),
+    data[0].value
+  );
+
+  return { max: maximum, min: minimum };
+}
+
+function selectWordScale(minMaxValues, type) {
+  if (type === "linear") {
+    const wordScaleLinear = d3
+      .scaleLinear()
+      .domain([minMaxValues.min, minMaxValues.max])
+      .range([10, 100]);
+    return wordScaleLinear;
+  } else if (type === "log") {
+    const wordScaleLog = d3
+      .scaleLog()
+      .domain([minMaxValues.min, minMaxValues.max])
+      .range([10, 100]);
+    return wordScaleLog;
+  }
+}
+
+const WordCloudDraggableDualLang = ({
+  data,
+  wordSizeMultiplier,
+  scaleType,
+}) => {
+  const [tooltip, showTooltip] = useState(true);
+
+  const minMax = findMaxMin(data);
+
+  const wordScale = selectWordScale(
+    minMax,
+    scaleType === undefined ? "linear" : scaleType
+  );
+
+  // Categories in our dataset
+  const categories = Array.from(new Set(data.map((item) => item.category)));
+
+  // Create a color scale
+  const colorScale = d3.scaleOrdinal(d3.schemeCategory10).domain(categories);
+
+  const svgRef = useRef(null);
+
+  useEffect(() => {
+    drawWordCloud();
+    drawLegend();
+  }, []);
+
+  // Function to trigger the dblclick event on all words
+  const triggerDblClickOnAllWords = () => {
+    const svgNode = svgRef.current;
+    const textElements = svgNode.querySelectorAll("text");
+
+    textElements.forEach((textElement) => {
+      // Create a new event
+      const event = new MouseEvent("dblclick", {
+        bubbles: true,
+        cancelable: true,
+        view: window,
+      });
+
+      // Dispatch the event on the element
+      textElement.dispatchEvent(event);
+    });
+  };
+
+  const drawWordCloud = () => {
+    const svg = d3.select(svgRef.current);
+    const parent = svg.node().parentNode;
+    const width = parent.offsetWidth;
+    const height = parent.offsetHeight < 500 ? 500 : parent.offsetHeight;
+
+    svg
+      .append("rect")
+      .attr("width", "100%")
+      .attr("height", "100%")
+      .attr("fill", "white");
+
+    const layout = d3Cloud()
+      .size([width, height])
+      .words(
+        data.map((d) => ({
+          text: d.word,
+          translation: d.translation,
+          size: wordScale(d.value) * wordSizeMultiplier,
+          value: d.value,
+          category: d.category,
+        }))
+      )
+      .padding(5)
+      .rotate(0)
+      .fontSize((d) => d.size)
+      .on("end", draw);
+
+    layout.start();
+
+    function dragstarted(event, d) {
+      showTooltip(false);
+      d3.select(this).raise().attr("strokewidth", 3);
+    }
+
+    function dragged(event, d) {
+      d3.select(this).attr(
+        "transform",
+        "translate(" + [(d.x = event.x), (d.y = event.y)] + ")"
+      );
+    }
+
+    function dragended(event, d) {
+      showTooltip(true);
+      d3.select(this).attr("strokewidth", 1);
+    }
+
+    function draw(words) {
+      var g = svg
+        .attr("width", layout.size()[0])
+        .attr("height", layout.size()[1])
+        .append("g")
+        .attr(
+          "transform",
+          "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")"
+        );
+
+      var text = g
+        .selectAll("text")
+        .data(words)
+        .enter()
+        .append("text")
+        .style("font-size", function (d) {
+          return d.size + "px";
+        })
+        .style("fill", function (d) {
+          // Find the original data item that corresponds to this word
+          const originalDataItem = data.find((item) => item.word === d.text);
+
+          // Apply the color scale to this data item's category
+          return colorScale(originalDataItem.category);
+        })
+        .attr("text-anchor", "middle")
+        .attr(
+          "transform",
+          (d) => "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")"
+        )
+        .attr("cursor", "pointer")
+        .attr("data-tooltip-id", "word-info")
+        .attr("data-tooltip-html", (d) => {
+          return `<div><p style="font-size:1.5em; font-weight: 600;">${d.text}/${d.translation}</p><p>Category: ${d.category}</p><p>Value: ${d.value}</p></div>`;
+        })
+        .text(function (d) {
+          return d.text;
+        })
+        .on("dblclick", function (d, i) {
+          console.log(this.innerHTML);
+          if (this.innerHTML === i.translation) {
+            d3.select(this).text(i.text);
+          } else {
+            d3.select(this).text(i.translation);
+          }
+        });
+      var drag = d3
+        .drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended);
+
+      text.call(drag);
+
+      // Run the simulation for a few iterations to let the words settle without overlapping
+    }
+  };
+  const drawLegend = () => {
+    // Append the div for the legend
+    const legendDiv = d3
+      .select(svgRef.current.parentNode) // select the parent node of the SVG.
+      .append("div")
+      .attr("class", "legend flex justify-center flex-wrap py-4 bg-stone-200"); // apply a class for styling.
+
+    // Append an item in the legend for each category
+    const legendItems = legendDiv
+      .selectAll(".legend-item")
+      .data(categories)
+      .enter()
+      .append("div")
+      .attr("class", "legend-item flex align-center mx-2"); // apply a class for styling.
+
+    // Append a color swatch for each item
+    legendItems
+      .append("div")
+      .style("width", "20px")
+      .style("height", "20px")
+      .style("display", "inline-block")
+      .style("background-color", function (d) {
+        return colorScale(d);
+      });
+
+    // Append a label for each item
+    legendItems
+      .append("text")
+      .text(function (d) {
+        return d;
+      })
+      .style("display", "inline-block")
+      .style("margin-left", "5px"); // give it some space from the color swatch.
+  };
+
+  return (
+    <div>
+      <div className="text-center">
+        <button
+          className="px-4 py-2 bg-blue-600 hover:bg-blue-800 rounded text-white inline-block mb-1 text-sm font-bold"
+          onClick={() => triggerDblClickOnAllWords()}
+        >
+          Translate All Words
+        </button>
+      </div>
+
+      <svg ref={svgRef}></svg>
+      {tooltip && (
+        <Tooltip
+          id="word-info"
+          variant="light"
+          opacity={0.9}
+          style={{ fontSize: "0.8em" }}
+        />
+      )}
+    </div>
+  );
+};
+
+export default WordCloudDraggableDualLang;
