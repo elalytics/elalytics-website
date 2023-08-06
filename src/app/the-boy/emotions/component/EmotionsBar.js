d25f99abad42a2573f65644044e4b8266cd099d0
ddeepak95 <ddeepak95@gmail.com>
2023-08-06 16:10:19 -0700

    emotions chart

---
+++ src/app/the-boy/emotions/component/EmotionsBar.js
@@ -0,0 +1,60 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useRef, useState } from "react";
+import { Chart } from "chart.js/auto";
+
+const EmotionsChart = (props) => {
+  const [chartData, setChartData] = useState();
+
+  const chartContainer = useRef(null);
+  const chartInstance = useRef(null);
+
+  useEffect(() => {
+    setChartData(props.data);
+  }, [props.data]);
+
+  useEffect(() => {
+    if (chartContainer && chartContainer.current) {
+      const myChartRef = chartContainer.current.getContext("2d");
+      if (chartInstance.current) {
+        // if a chart instance exists
+        chartInstance.current.destroy(); // destroy it
+      }
+
+      chartInstance.current = new Chart(myChartRef, {
+        type: "bar",
+        data: chartData,
+        options: {
+          indexAxis: props.axis,
+          scale: {
+            ticks: {
+              precision: 0,
+            },
+          },
+          scales: {
+            y: {
+              beginAtZero: true,
+              grace: "5%",
+            },
+            x: {
+              grace: "5%",
+              stacked: props.stacked === undefined ? false : props.stacked,
+            },
+          },
+          responsive: true,
+        },
+      });
+    }
+  }, [chartContainer, chartData]);
+
+  if (props.data === undefined || props.data === null) {
+    return <div>loading</div>;
+  }
+
+  return (
+    <div style={{ width: "100%" }}>
+      <canvas ref={chartContainer} />
+    </div>
+  );
+};
+
+export default EmotionsChart;
---
+++ src/app/the-boy/emotions/page.js
@@ -0,0 +1,85 @@
+"use client";
+
+import EmotionsChart from "./component/EmotionsBar";
+import data from "./data/emotions_by_character.json";
+
+function convertEmotionsDataBasedOnCharacters(input) {
+  const labels = Object.keys(input); // Extract the characters
+
+  // Automatically extract emotion categories from one of the characters (e.g., "Mother")
+  const emotions = Object.keys(input[labels[0]]);
+
+  const datasets = emotions.map((emotion) => {
+    return {
+      label: capitalizeFirstLetter(emotion),
+      data: labels.map((character) => input[character][emotion]),
+    };
+  });
+
+  return {
+    labels,
+    datasets,
+  };
+}
+
+function convertCharactersDataBasedOnEmotionsStack(input) {
+  // Extract characters and emotions from the data in format A
+  const characters = Object.keys(input);
+  const emotions = Object.keys(input[characters[0]]);
+
+  // Map each character to their emotion data
+  const datasets = characters.map((character) => {
+    return {
+      label: character,
+      data: emotions.map((emotion) => input[character][emotion]),
+      stack: "Stack 0",
+    };
+  });
+
+  // Format labels with capitalized emotion names
+  const labels = emotions.map(capitalizeFirstLetter);
+
+  return {
+    labels,
+    datasets,
+  };
+}
+
+// Helper function to capitalize the first letter of a string
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+export default function Home() {
+  return (
+    <main>
+      <div className="h-screen overflow-x-hidden bg-gray-100">
+        <div className="my-8 text-center max-w-3xl m-auto">
+          <span className="px-4 py-1 bg-stone-600 rounded text-white inline-block mb-1 text-sm font-bold">
+            Boy
+          </span>
+          <h1 className="text-3xl font-bold ">Emotions</h1>
+        </div>
+
+        <div className="max-w-5xl m-auto mb-12">
+          <div>
+            <h2 className="text-center text-xl">Most Common Emotions</h2>
+            <EmotionsChart
+              data={convertCharactersDataBasedOnEmotionsStack(data)}
+              stacked={true}
+              axis="y"
+            />
+          </div>
+          <div className="my-12">
+            <h2 className="text-center text-xl">
+              Emotion Distribution Within Characters
+            </h2>
+            <EmotionsChart
+              data={convertEmotionsDataBasedOnCharacters(data)}
+              axis="x"
+            />
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
