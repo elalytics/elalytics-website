db70b18997884469a2e4fe061e7918b0b66b85ee
ddeepak95 <ddeepak95@gmail.com>
2023-08-24 13:08:21 -0700

    grammar correction

--- src/app/general/definitions.js
+++ src/app/general/definitions.js
@@ -1,2 +1,2 @@
 export const conflictDefinition =
-  "The conflict score is calculated by breaking each paragraph down into sentences. Every sentence get's a computed 'negativity' and 'intensity' score, and then this is averaged to give each paragraph a conflict score, with the idea being the more negative and intense the emotions in a paragraph are, the higher the conflict is. A higher score == more conflict.";
+  "The conflict score is calculated by breaking each paragraph down into sentences. Every sentence gets a computed 'negativity' and 'intensity' score, and then this is averaged to give each paragraph a conflict score, with the idea being the more negative and intense the emotions in a paragraph are, the higher the conflict is. A higher score == more conflict.";
