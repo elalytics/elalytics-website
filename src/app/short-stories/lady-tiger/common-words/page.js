e3414de72bc90a88a2aee72811c23b6dcadb3b91
ddeepak95 <ddeepak95@gmail.com>
2023-10-16 21:24:51 -0700

    lady or tiger viz

---
+++ src/app/short-stories/lady-tiger/common-words/page.js
@@ -0,0 +1,20 @@
+import data from "@/app/data-processing/src/book_projects/lady_tiger/data/wcloud.json";
+import WordCloudDraggableAndBilingual from "@/app/utils/charts/WordCloudDraggableAndBilingual";
+
+export const metadata = {
+  title: `Common Words Wordcloud | The Lady, or the Tiger? | Elalytics`,
+};
+
+export default function App() {
+  const wordsToRemoveData = ["the"];
+  const categoriesToRemoveData = [];
+  return (
+    <WordCloudDraggableAndBilingual
+      data={data}
+      bookName="The Lady, or the Tiger?"
+      wordCloudTitle="Common Words"
+      wordsToRemove={wordsToRemoveData}
+      categoriesToRemove={categoriesToRemoveData}
+    />
+  );
+}
