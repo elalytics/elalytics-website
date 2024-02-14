e7f7fdc6c7e095281e08630db413de2dba1e31e4
ddeepak95 <ddeepak95@gmail.com>
2024-02-14 10:49:24 -0800

    firestore updated

--- src/app/utils/firebaseApp.js
+++ src/app/utils/firebaseApp.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://support.google.com/firebase/answer/7015592
 const firebaseConfig = {
-  apiKey: "AIzaSyB0T4MHs9EA0onOdjdgLZupOYywrMZD_Gk",
-  authDomain: "elalytics.firebaseapp.com",
-  projectId: "elalytics",
-  storageBucket: "elalytics.appspot.com",
-  messagingSenderId: "493599989080",
-  appId: "1:493599989080:web:e8e873f61c575f17e8d92d",
+  apiKey: "AIzaSyChm9rF1bTCtUxW52P_cmyxlgZnzNa8KCQ",
+  authDomain: "gse-elalytics.firebaseapp.com",
+  projectId: "gse-elalytics",
+  storageBucket: "gse-elalytics.appspot.com",
+  messagingSenderId: "447788620866",
+  appId: "1:447788620866:web:e765be60ea1862c86561db",
 };
 
 // Initialize Firebase
