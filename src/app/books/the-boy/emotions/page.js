6c4fbbeb55b8df9753f9929c6cc119c2282dcab8
ddeepak95 <ddeepak95@gmail.com>
2024-05-22 12:18:01 -0700

    emotions page

--- src/app/books/the-boy/emotions/page.js
+++ src/app/books/the-boy/emotions/page.js
@@ -2,6 +2,9 @@
 
 import EmotionsChart from "./component/EmotionsBar";
 import data from "./data/emotions_by_character.json";
+import VisualizationDescription from "@/app/utils/components/VisualizationDescription";
+import SuggestedRoutines from "@/app/utils/components/SuggestedRoutines";
+import Link from "next/link";
 
 function convertEmotionsDataBasedOnCharacters(input) {
   const labels = Object.keys(input); // Extract the characters
@@ -59,7 +62,47 @@ export default function Home() {
           </span>
           <h1 className="text-3xl font-bold ">Emotions</h1>
         </div>
-
+        <VisualizationDescription>
+          <p>
+            These visualizations use an emotions classifier. The{" "}
+            <strong>emotions classifier</strong> is a machine learning model
+            that assigns emotion scores to a text input. In this case, each word
+            associated with the character gets an emotion score from the
+            classifier and the specific emotion with the highest score is
+            associated with that word. This visualization represents the
+            corresponding word-emotion distribution for the selected characters.
+            The scores are provided for the following emotions: anger, disgust,
+            fear, joy, neutral, sadness, surprise. You can experiment with the
+            emotions classifier here:{" "}
+            <a
+              href="https://huggingface.co/j-hartmann/emotion-english-distilroberta-base"
+              target="_blank"
+              className="text-cardinal-red hover:text-cardinal-red-dark underline"
+            >
+              https://huggingface.co/j-hartmann/emotion-english-distilroberta-base
+            </a>
+            <br />
+            This visualization doesn't contain stop words. Stop words are
+            commonly used words such as "the," "is," and "at". These words are
+            often filtered out in text processing and search queries to focus on
+            more descriptive words.
+          </p>
+        </VisualizationDescription>
+        <SuggestedRoutines>
+          <ul className="list-disc list-inside">
+            <li>
+              <Link
+                href={
+                  "https://docs.google.com/document/d/1VIlSlSZYwBjfMu3k8TB1AXRMInQPv4LvjV9mHqoeKi8"
+                }
+                target="_blank"
+                className="text-cardinal-red hover:text-cardinal-red-dark underline"
+              >
+                Big, Medium, Small
+              </Link>
+            </li>
+          </ul>
+        </SuggestedRoutines>
         <div className="max-w-5xl m-auto mb-12">
           <div>
             <h2 className="text-center text-xl">Most Common Emotions</h2>
