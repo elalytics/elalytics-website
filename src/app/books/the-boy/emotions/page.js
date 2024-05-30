bb4f29df4d76a380a9c762763ce84626154fd0fa
ddeepak95 <ddeepak95@gmail.com>
2024-05-30 11:08:20 -0700

    description style updated

--- src/app/books/the-boy/emotions/page.js
+++ src/app/books/the-boy/emotions/page.js
@@ -55,54 +55,47 @@ function capitalizeFirstLetter(string) {
 export default function Home() {
   return (
     <main>
-      <div className="h-screen overflow-x-hidden bg-gray-100">
+      <div className="overflow-x-hidden bg-gray-100">
         <div className="my-8 text-center max-w-3xl m-auto">
           <span className="px-4 py-1 bg-stone-dark rounded text-white inline-block mb-1 text-sm font-bold">
             Boy
           </span>
-          <h1 className="text-3xl font-bold ">Emotions</h1>
+          <h1 className="text-3xl font-bold ">
+            Emotions
+            <VisualizationDescription>
+              <div className="space-y-3">
+                <p>
+                  These visualizations use an emotions classifier. The{" "}
+                  <strong>emotions classifier</strong> is a machine learning
+                  model that assigns emotion scores to a text input. In this
+                  case, each word associated with the character gets an emotion
+                  score from the classifier and the specific emotion with the
+                  highest score is associated with that word. This visualization
+                  represents the corresponding word-emotion distribution for the
+                  selected characters. The scores are provided for the following
+                  emotions: anger, disgust, fear, joy, neutral, sadness,
+                  surprise. You can experiment with the emotions classifier
+                  here:{" "}
+                  <a
+                    href="https://huggingface.co/j-hartmann/emotion-english-distilroberta-base"
+                    target="_blank"
+                    className="text-cardinal-red hover:text-cardinal-red-dark underline"
+                  >
+                    https://huggingface.co/j-hartmann/emotion-english-distilroberta-base
+                  </a>
+                </p>
+                <p>
+                  This visualization doesn&apos;t contain stop words. Stop words
+                  are commonly used words such as &quot;the,&quot;
+                  &quot;is,&quot; and &quot;at&quot;. These words are often
+                  filtered out in text processing and search queries to focus on
+                  more descriptive words.
+                </p>
+              </div>
+            </VisualizationDescription>
+          </h1>
         </div>
-        <VisualizationDescription>
-          <p>
-            These visualizations use an emotions classifier. The{" "}
-            <strong>emotions classifier</strong> is a machine learning model
-            that assigns emotion scores to a text input. In this case, each word
-            associated with the character gets an emotion score from the
-            classifier and the specific emotion with the highest score is
-            associated with that word. This visualization represents the
-            corresponding word-emotion distribution for the selected characters.
-            The scores are provided for the following emotions: anger, disgust,
-            fear, joy, neutral, sadness, surprise. You can experiment with the
-            emotions classifier here:{" "}
-            <a
-              href="https://huggingface.co/j-hartmann/emotion-english-distilroberta-base"
-              target="_blank"
-              className="text-cardinal-red hover:text-cardinal-red-dark underline"
-            >
-              https://huggingface.co/j-hartmann/emotion-english-distilroberta-base
-            </a>
-            <br />
-            This visualization doesn&apos;t contain stop words. Stop words are
-            commonly used words such as &quot;the,&quot; &quot;is,&quot; and
-            &quot;at&quot;. These words are often filtered out in text
-            processing and search queries to focus on more descriptive words.
-          </p>
-        </VisualizationDescription>
-        <SuggestedRoutines>
-          <ul className="list-disc list-inside">
-            <li>
-              <Link
-                href={
-                  "https://docs.google.com/document/d/1VIlSlSZYwBjfMu3k8TB1AXRMInQPv4LvjV9mHqoeKi8"
-                }
-                target="_blank"
-                className="text-cardinal-red hover:text-cardinal-red-dark underline"
-              >
-                Big, Medium, Small
-              </Link>
-            </li>
-          </ul>
-        </SuggestedRoutines>
+
         <div className="max-w-5xl m-auto mb-12">
           <div>
             <h2 className="text-center text-xl">Most Common Emotions</h2>
--- src/app/utils/components/VisualizationDescription.js
+++ src/app/utils/components/VisualizationDescription.js
@@ -1,5 +1,8 @@
 "use client";
 import React, { useState } from "react";
+import { Tooltip } from "react-tooltip";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 
 const VisualizationDescription = ({ children }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -9,50 +12,19 @@ const VisualizationDescription = ({ children }) => {
   };
 
   return (
-    <div
-      id="accordion-collapse"
-      data-accordion="collapse"
-      className="max-w-3xl mx-auto mb-2"
-    >
-      <h2 id="accordion-collapse-heading-1">
-        <button
-          type="button"
-          className={`flex ${
-            isOpen ? "bg-gray-50" : "bg-white rounded-b"
-          } items-center justify-between w-full rounded-t px-5 py-1 font-medium rtl:text-right text-gray-500 border border-gray-200 hover:bg-gray-50 gap-3`}
-          onClick={toggleAccordion}
-          aria-expanded={isOpen}
-          aria-controls="accordion-collapse-body-1"
-        >
-          <span>About the visualization</span>
-          <svg
-            data-accordion-icon
-            className={`w-3 h-3 ${isOpen ? "" : "rotate-180"} shrink-0`}
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 10 6"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M9 5 5 1 1 5"
-            />
-          </svg>
-        </button>
-      </h2>
-      <div
-        id="accordion-collapse-body-1"
-        className={isOpen ? "" : "hidden"}
-        aria-labelledby="accordion-collapse-heading-1"
-      >
-        <div className="p-5 border border-gray-200 bg-white rounded-b">
+    <span>
+      <span data-tooltip-id="info">
+        <FontAwesomeIcon
+          className="text-black-50 hover:text-black-80 px-1 cursor-help"
+          icon={faInfoCircle}
+        />
+      </span>
+      <Tooltip id="info" variant="light" clickable>
+        <div className="max-w-2xl text-base font-normal text-left">
           {children}
         </div>
-      </div>
-    </div>
+      </Tooltip>
+    </span>
   );
 };
 
