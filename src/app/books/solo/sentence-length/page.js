8699c79b68fc24c294e482733d43c378ce585763
KGeorgii <87044534+KGeorgii@users.noreply.github.com>
2024-09-12 12:23:47 -0700

    Uploading Maroo, Going Solo and To Kill a Mockingbird

---
+++ src/app/books/solo/sentence-length/page.js
@@ -0,0 +1,9 @@
+import App from "./WorkingFile";
+
+export const metadata = {
+  title: "Sentence Length Chart | Annabel Lee | Elalytics",
+};
+
+export default function Home() {
+  return <App />;
+}
