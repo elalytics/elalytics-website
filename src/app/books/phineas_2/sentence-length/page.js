85dcaf48ef0735818788a71206ce3da1c14733f1
KGeorgii <87044534+KGeorgii@users.noreply.github.com>
2024-11-01 00:02:07 -0700

    Added Phineas Gage

---
+++ src/app/books/phineas_2/sentence-length/page.js
@@ -0,0 +1,9 @@
+import App from "./WorkingFile";
+
+export const metadata = {
+  title: "Sentence Length Chart | Phineas Gage | Elalytics",
+};
+
+export default function Home() {
+  return <App />;
+}
