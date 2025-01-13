4a3263c45395ac76dc44555158aae24eae329c99
KGeorgii <87044534+KGeorgii@users.noreply.github.com>
2025-01-13 00:44:36 -0800

    Deployment fix

--- src/app/books/frankenstein/sentence-length/page.js
+++
@@ -1,9 +0,0 @@
-import App from "./WorkingFile";
-
-export const metadata = {
-  title: "Sentence Length Chart | Frankenstein | Elalytics",
-};
-
-export default function Home() {
-  return <App />;
-}
--- src/app/books/ultima/sentence-length/page.js
+++
@@ -1,9 +0,0 @@
-import App from "./WorkingFile";
-
-export const metadata = {
-  title: "Sentence Length Chart | Bless Me, Ultima | Elalytics",
-};
-
-export default function Home() {
-  return <App />;
-}
